ba8915db4a6b01b4441fc9940e0829afe42721f8
Artem Vinogradov <vinogradovart88@gmail.com>
2022-03-01 14:20:55 +0300

    add UI component Card

---
+++ src/componets/UI/CardComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card">
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }}</span>
+    <div class="card-body">card-body</div>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      require: true
+    },
+    title: {
+      type: String,
+      require: true
+    }
+  }
+}
+</script>
--- src/pages/HomeComponent.vue
+++ src/pages/HomeComponent.vue
@@ -1,3 +1,59 @@
 <template>
-    <p>Home</p>
+  <div class="md-body">
+    <div class="card__wrapper" v-for="item in items" :key="item.id">
+      <card-component
+        :name="`${item.lvl} lvl`"
+        :title="item.title"
+        :disc="item.descr"
+      >
+        <h1>Hello</h1>
+        <p>Edit me</p>
+      </card-component>
+    </div>
+  </div>
 </template>
+
+<script>
+import CardComponent from "@/componets/UI/CardComponent"
+
+export default {
+  components: { CardComponent },
+
+  data() {
+    return {
+      items: [
+        {
+          id: 1,
+          title: "Archer",
+          descr: "archer archer archer archer",
+          lvl: 5
+        },
+        {
+          id: 2,
+          title: "Barbarian",
+          descr: "barbarian barbarian barbarian",
+          lvl: 5
+        },
+        {
+          id: 3,
+          title: "Giant",
+          descr: "giant giant giant giant",
+          lvl: 5
+        },
+        {
+          id: 4,
+          title: "Goblin",
+          descr: "goblin goblin goblin goblin",
+          lvl: 5
+        },
+        {
+          id: 5,
+          title: "Wizard",
+          descr: "wizard wizard wizard wizard",
+          lvl: 5
+        }
+      ]
+    }
+  }
+}
+</script>
